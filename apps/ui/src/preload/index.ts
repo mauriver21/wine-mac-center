8b67a27de0baa360cd5cd45cd44d0db378547acb
Mauricio Rivera <mauriver21@gmail.com>
2025-07-28 12:59:40 -0500

    Relocate RendererApi definition

--- apps/ui/src/preload/index.ts
+++ apps/ui/src/preload/index.ts
@@ -1,37 +1,7 @@
 import { contextBridge, ipcRenderer } from 'electron';
 import { electronAPI } from '@electron-toolkit/preload';
-import { PathOrFileDescriptor } from 'fs';
 import { ElectronApi } from '../main/types/ElectronApi';
-import { dialog } from 'electron';
-
-export type Api = {
-  getAppPath: () => Promise<string>;
-  execCommand: (cmd: string) => Promise<{
-    stdOut: string;
-    stdErr: string;
-  }>;
-  pathJoin: (...paths: string[]) => Promise<string>;
-  spawnProcess: (
-    command: string,
-    action?: { type: string; data: string }
-  ) => Promise<{ pid: number }>;
-  fileExists: (path: string) => Promise<boolean>;
-  writeFile: (file: PathOrFileDescriptor, data: string) => void;
-  readDirectory: (dirPath: string) => Promise<string[]>;
-  dirExists: (dirPath: string) => Promise<boolean>;
-  readBinaryFile: (filePath: string) => Promise<Buffer>;
-  createDirectory: (dirPath: string) => Promise<void>;
-  readFileAsString: (filePath: string) => Promise<string>;
-  writeBinaryFile: (filePath: string, arrayBuffer: ArrayBuffer) => Promise<void>;
-  showOpenDialog: typeof dialog.showOpenDialog;
-  onStdOut: (callback: (data: string) => void) => void;
-  onStdErr: (callback: (data: string) => void) => void;
-  onExit: (callback: (code: number) => void) => void;
-};
-
-type RendererApi = {
-  [K in keyof Api]: (...args: Parameters<Api[K]>) => ReturnType<Api[K]>;
-};
+import { RendererApi } from './types/RendererApi';
 
 // Custom APIs for renderer
 const api: RendererApi = {
---
+++ apps/ui/src/preload/types/RendererApi.ts
@@ -0,0 +1,31 @@
+import { dialog } from 'electron';
+import { PathOrFileDescriptor } from 'fs';
+
+type Api = {
+  getAppPath: () => Promise<string>;
+  execCommand: (cmd: string) => Promise<{
+    stdOut: string;
+    stdErr: string;
+  }>;
+  pathJoin: (...paths: string[]) => Promise<string>;
+  spawnProcess: (
+    command: string,
+    action?: { type: string; data: string }
+  ) => Promise<{ pid: number }>;
+  fileExists: (path: string) => Promise<boolean>;
+  writeFile: (file: PathOrFileDescriptor, data: string) => void;
+  readDirectory: (dirPath: string) => Promise<string[]>;
+  dirExists: (dirPath: string) => Promise<boolean>;
+  readBinaryFile: (filePath: string) => Promise<Buffer>;
+  createDirectory: (dirPath: string) => Promise<void>;
+  readFileAsString: (filePath: string) => Promise<string>;
+  writeBinaryFile: (filePath: string, arrayBuffer: ArrayBuffer) => Promise<void>;
+  showOpenDialog: typeof dialog.showOpenDialog;
+  onStdOut: (callback: (data: string) => void) => void;
+  onStdErr: (callback: (data: string) => void) => void;
+  onExit: (callback: (code: number) => void) => void;
+};
+
+export type RendererApi = {
+  [K in keyof Api]: (...args: Parameters<Api[K]>) => ReturnType<Api[K]>;
+};
