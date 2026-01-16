e4e473113b7b6631d82cf5d40af72369951a0ae0
Mauricio Rivera <mauriver21@gmail.com>
2026-01-15 23:01:19 -0500

    Stable installSteamCMD

--- apps/ui/resources/clients/steam/installSteamCMD.sh
+++ apps/ui/resources/clients/steam/installSteamCMD.sh
@@ -1,5 +1,5 @@
 installSteamCMD() {
-    local STEAM_CLI_PATH="$CLIENTS_PATH/steamcmd"    
+    local STEAM_CLI_PATH="$CLIENTS_PATH/steam"
     cd "$STEAM_CLI_PATH"
     curl -sL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_osx.tar.gz" | tar zxvf -
 }
