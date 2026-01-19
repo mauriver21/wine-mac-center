60660c702d9596b295a5ab15e36f9f4379c08e0a
Mauricio Rivera <mauriver21@gmail.com>
2026-01-19 02:20:34 -0500

    Add contents area to config layout

--- apps/ui/src/renderer/src/layouts/ConfigLayout/index.tsx
+++ apps/ui/src/renderer/src/layouts/ConfigLayout/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { ButtonProps } from '@components/Button';
-import { Box, H6, Button } from 'reactjs-shared-ui';
+import { Box, H6, Button, ContentsAreaHandle, ContentsArea } from 'reactjs-shared-ui';
 
 export interface ConfigLayoutProps {
   mainTitle: string;
@@ -13,31 +13,43 @@ export const ConfigLayout: React.FC<ConfigLayoutProps> = ({
   backButtonProps,
   contentSlot
 }) => {
+  const contentsAreaRef = useRef<ContentsAreaHandle>(null);
+
   return (
-    <Box display="grid" overflow="auto" gridTemplateRows="auto 1fr">
-      <Box
-        p={2}
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        sx={{
-          boxShadow: (theme) => `inset 0 -1px ${theme.palette.secondary.main}`
+    <Box display="grid" overflow="auto">
+      <ContentsArea
+        ref={contentsAreaRef}
+        style={{
+          height: '100%',
+          display: 'grid',
+          overflow: 'auto',
+          gridTemplateRows: 'auto 1fr'
         }}
       >
-        <H6 color="text.secondary" fontWeight={500}>
-          {mainTitle}
-        </H6>
-        <Button
-          sx={{ border: (theme) => `1px solid ${theme.palette.primary.dark}` }}
-          color="secondary"
-          {...backButtonProps}
+        <Box
+          p={2}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          sx={{
+            boxShadow: (theme) => `inset 0 -1px ${theme.palette.secondary.main}`
+          }}
         >
-          Back
-        </Button>
-      </Box>
-      <Box display="grid" overflow="auto">
-        {contentSlot}
-      </Box>
+          <H6 color="text.secondary" fontWeight={500}>
+            {mainTitle}
+          </H6>
+          <Button
+            sx={{ border: (theme) => `1px solid ${theme.palette.primary.dark}` }}
+            color="secondary"
+            {...backButtonProps}
+          >
+            Back
+          </Button>
+        </Box>
+        <Box display="grid" overflow="auto">
+          {contentSlot}
+        </Box>
+      </ContentsArea>
     </Box>
   );
 };
