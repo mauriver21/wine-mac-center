4533fa5aae3f9c2120e918dea67b77adf08ee5d6
Mauricio Rivera <mauriver21@gmail.com>
2026-01-19 02:25:41 -0500

    Add table of contents

--- apps/ui/src/renderer/src/layouts/ConfigLayout/index.tsx
+++ apps/ui/src/renderer/src/layouts/ConfigLayout/index.tsx
@@ -1,17 +1,27 @@
 import React, { useRef } from 'react';
 import { ButtonProps } from '@components/Button';
-import { Box, H6, Button, ContentsAreaHandle, ContentsArea } from 'reactjs-shared-ui';
+import {
+  Box,
+  H6,
+  Button,
+  ContentsAreaHandle,
+  ContentsArea,
+  TableOfContents
+} from 'reactjs-shared-ui';
+import { alpha } from '@mui/material';
 
 export interface ConfigLayoutProps {
   mainTitle: string;
   backButtonProps?: ButtonProps;
   contentSlot?: React.ReactNode;
+  showTableOfContents?: boolean;
 }
 
 export const ConfigLayout: React.FC<ConfigLayoutProps> = ({
   mainTitle,
   backButtonProps,
-  contentSlot
+  contentSlot,
+  showTableOfContents = true
 }) => {
   const contentsAreaRef = useRef<ContentsAreaHandle>(null);
 
@@ -46,8 +56,29 @@ export const ConfigLayout: React.FC<ConfigLayoutProps> = ({
             Back
           </Button>
         </Box>
-        <Box display="grid" overflow="auto">
-          {contentSlot}
+        <Box
+          display="grid"
+          gridTemplateColumns={showTableOfContents ? '1fr 250px' : '1fr'}
+          overflow="auto"
+        >
+          <Box
+            overflow="auto"
+            display="grid"
+            sx={{
+              '&::-webkit-scrollbar-thumb': {
+                backgroundColor: (theme) => alpha(theme.palette?.primary.dark, 0.3)
+              }
+            }}
+          >
+            <Box display="grid" overflow="auto">
+              {contentSlot}
+            </Box>
+          </Box>
+          {showTableOfContents && (
+            <Box borderLeft={(theme) => `1px solid ${theme.palette.secondary.light}`}>
+              <TableOfContents pt={1} />
+            </Box>
+          )}
         </Box>
       </ContentsArea>
     </Box>
