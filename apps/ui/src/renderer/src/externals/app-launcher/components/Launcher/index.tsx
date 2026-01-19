695833b735db43a4ba5692ce438e123fd3d4f1d1
Mauricio Rivera <mauriver21@gmail.com>
2026-01-18 20:59:03 -0500

    WIP app launcher ui component

---
+++ apps/ui/src/renderer/src/externals/app-launcher/components/Launcher/index.tsx
@@ -0,0 +1,9 @@
+import { Box } from 'reactjs-shared-ui';
+
+export const Launcher: React.FC = () => {
+  return (
+    <Box width="100%" height="100%">
+      App Launcher Works!!
+    </Box>
+  );
+};
