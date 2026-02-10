74418cba2c349951d5149c5a986139f92c2d11e7
Mauricio Rivera <mauriver21@gmail.com>
2026-02-09 19:38:39 -0500

    Add show in finder for Wine Engines

--- apps/ui/src/renderer/src/components/DownloadWineEngines/index.tsx
+++ apps/ui/src/renderer/src/components/DownloadWineEngines/index.tsx
@@ -7,15 +7,25 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Icon, Stack } from 'reactjs-shared-ui';
 import { TextField } from 'reactjs-shared-ui/forms';
+import { showItemInFolder } from '@utils/showItemInFolder';
+import { IconButton } from '@components/IconButton';
+import { Folder } from '@mui/icons-material';
+import { useEnv } from '@hooks/useEnv';
 
 export interface DownloadWineEnginesProps {}
 
 export const DownloadWineEngines: React.FC<DownloadWineEnginesProps> = () => {
+  const env = useEnv();
+  const { WINE_ENGINES_PATH } = env.get();
   const [downloadQueue, setDownloadQueue] = useState<string[]>([]);
   const configLayout = useConfigLayout();
   const wineEngineModel = useWineEngineModel();
   const wineEngines = useSelector(wineEngineModel.selectWineEngines);
   const wineEnginesDownloadables = useSelector(wineEngineModel.selectWineEnginesDownloadables);
+
+  const showEngineLocation = (engineVersion: string) => {
+    showItemInFolder(`${WINE_ENGINES_PATH}/${engineVersion}.tar.7z`);
+  };
 
   useEffect(() => {
     wineEngineModel.listDownloadables();
@@ -28,23 +38,49 @@ export const DownloadWineEngines: React.FC<DownloadWineEnginesProps> = () => {
   return (
     <CardItem icon={CpuChipIcon} label="Wine Engines">
       <Stack spacing={2}>
-        {wineEnginesDownloadables?.map((item, key) => (
-          <TextField
-            key={key}
-            InputProps={{
-              readOnly: true,
-              endAdornment: wineEngines?.includes(item.version) ? (
-                <Icon pr={1} title="Engine Downloaded" color="success.main" render={CheckIcon} />
-              ) : (
-                <DownloadEngineButton
-                  setDownloadQueue={setDownloadQueue}
-                  wineEngineDownloadable={item}
-                />
-              )
-            }}
-            value={item.version}
-          />
-        ))}
+        {wineEnginesDownloadables?.map((item, key) => {
+          const isDownloaded = wineEngines?.includes(item.version);
+
+          return (
+            <TextField
+              sx={{
+                '&': { '.engine-location': { display: 'none' } },
+                '&:hover': {
+                  '.downloaded-icon': { display: 'none' },
+                  '.engine-location': { display: 'block' }
+                }
+              }}
+              key={key}
+              InputProps={{
+                readOnly: true,
+                endAdornment: isDownloaded ? (
+                  <>
+                    <Icon
+                      className="downloaded-icon"
+                      pr={1}
+                      title="Engine Downloaded"
+                      color="success.main"
+                      render={CheckIcon}
+                    />
+                    <IconButton
+                      className="engine-location"
+                      title="Open Location"
+                      onClick={() => showEngineLocation(item.version)}
+                    >
+                      <Icon render={Folder} />
+                    </IconButton>
+                  </>
+                ) : (
+                  <DownloadEngineButton
+                    setDownloadQueue={setDownloadQueue}
+                    wineEngineDownloadable={item}
+                  />
+                )
+              }}
+              value={item.version}
+            />
+          );
+        })}
       </Stack>
     </CardItem>
   );
