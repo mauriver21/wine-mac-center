4d0160b1fc02db86e8eb06bde7a41f7a6d967fbc
Mauricio Rivera <mauriver21@gmail.com>
2026-05-20 14:36:22 -0500

    Add i18n provider in app launcher

--- apps/ui/src/renderer/src/externals/app-launcher/App.tsx
+++ apps/ui/src/renderer/src/externals/app-launcher/App.tsx
@@ -2,6 +2,8 @@ import { useRoutes } from 'react-router-dom';
 import { routes } from '@app-launcher/routes';
 import { useResolveAppName } from '@hooks/useResolveAppName';
 import { WineAppProvider, WineAppProviderProps } from '@components/WineAppProvider';
+import { I18nProvider } from 'reactjs-shared-ui/i18next';
+import * as resources from '@i18n/translations';
 
 export interface AppProps {
   onInitialized?: WineAppProviderProps['onInitialized'];
@@ -11,12 +13,14 @@ export interface AppProps {
 export const App: React.FC<AppProps> = ({ onInitialized, onUpdateAppLauncherConfig }) => {
   const appName = useResolveAppName();
   return (
-    <WineAppProvider
-      onInitialized={onInitialized}
-      onUpdateAppLauncherConfig={onUpdateAppLauncherConfig}
-      appName={appName}
-    >
-      {useRoutes(routes)}
-    </WineAppProvider>
+    <I18nProvider language="es" resources={resources}>
+      <WineAppProvider
+        onInitialized={onInitialized}
+        onUpdateAppLauncherConfig={onUpdateAppLauncherConfig}
+        appName={appName}
+      >
+        {useRoutes(routes)}
+      </WineAppProvider>
+    </I18nProvider>
   );
 };
