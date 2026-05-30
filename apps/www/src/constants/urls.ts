0e6c7b44bb304b36ec775b928fae8ab7ab0a5928
Mauricio Rivera <mauriver21@gmail.com>
2026-05-30 11:31:41 -0500

    Fix base url

--- apps/www/src/constants/urls.ts
+++ apps/www/src/constants/urls.ts
@@ -1,4 +1,4 @@
 export const GITHUB_URL = 'https://github.com/mauriver21';
-export const BASE_URL = 'wine-mac-center';
+export const BASE_URL = import.meta.env.BASE_URL;
 export const REPOSITORY_URL = `https://github.com/mauriver21/${BASE_URL}`;
 export const RELEASES_URL = `${REPOSITORY_URL}/releases`;
--- apps/www/src/main.tsx
+++ apps/www/src/main.tsx
@@ -1,17 +1,16 @@
 import './index.css';
 import 'reactjs-shared-ui/styles.css';
 import { App } from './App.tsx';
-import { BASE_URL } from '@constants/urls.ts';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { I18nProvider } from 'reactjs-shared-ui/i18next';
-import { IS_DEV } from '@constants/envs.ts';
 import { ThemeProvider } from 'reactjs-shared-ui';
 import * as resources from '@i18n/translations';
+import { BASE_URL } from '@constants/urls.ts';
 
 createRoot(document.getElementById('root')!).render(
   <I18nProvider resources={resources}>
-    <HashRouter basename={IS_DEV ? '' : BASE_URL}>
+    <HashRouter basename={BASE_URL}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
