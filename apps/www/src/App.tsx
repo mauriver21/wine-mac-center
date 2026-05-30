e6f9a6188ee06c7ab50ab61716746d85f291b3f1
Mauricio Rivera <mauriver21@gmail.com>
2026-05-30 12:16:35 -0500

    Remove base from HashRouter

--- apps/www/src/App.tsx
+++ apps/www/src/App.tsx
@@ -1,15 +1,6 @@
 import { routes } from '@routes';
-import { useEffect } from 'react';
-import { useNavigate, useRoutes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 
 export const App = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (import.meta.env.PROD) {
-      navigate('/#');
-    }
-  }, []);
-
   return useRoutes(routes);
 };
--- apps/www/src/main.tsx
+++ apps/www/src/main.tsx
@@ -6,11 +6,10 @@ import { HashRouter } from 'react-router-dom';
 import { I18nProvider } from 'reactjs-shared-ui/i18next';
 import { ThemeProvider } from 'reactjs-shared-ui';
 import * as resources from '@i18n/translations';
-import { BASE_URL } from '@constants/urls.ts';
 
 createRoot(document.getElementById('root')!).render(
   <I18nProvider resources={resources}>
-    <HashRouter basename={BASE_URL}>
+    <HashRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
