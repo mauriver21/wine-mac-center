ec56d565ea4085d059d9a233a388a862cc2cdeed
Mauricio Rivera <mauriver21@gmail.com>
2026-07-19 14:50:43 -0500

    fix wine paths

--- apps/ui/resources/bash/wineEnv.sh
+++ apps/ui/resources/bash/wineEnv.sh
@@ -1,8 +1,13 @@
 wineEnv() {
-  if [[ -e "$WINE_APP_BIN_PATH/wine32on64" ]]; then
+  if [[ -x "$WINE_APP_BIN_PATH/wine32on64" ]]; then
     WINE_PATH=$WINE_APP_BIN_PATH/wine32on64
-  else
+  elif [[ -x "$WINE_APP_BIN_PATH/wine64" ]]; then
     WINE_PATH=$WINE_APP_BIN_PATH/wine64
+  elif [[ -x "$WINE_APP_BIN_PATH/wine" ]]; then
+    WINE_PATH=$WINE_APP_BIN_PATH/wine
+  else
+    echo "Error: No executable Wine binary found in $WINE_APP_BIN_PATH"
+    exit 1
   fi
 
   DYLD_FALLBACK_LIBRARY_PATH="${DYLD_FALLBACK_LIBRARY_PATH}:$WINE_APP_FRAMEWORKS_PATH" \
